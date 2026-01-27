97d473c7072a1c767e3c1067eb7b6a61cedebe68
FakeKtOshi <KritZaZalol@gmail.com>
2026-01-28 03:41:29 +0700

    twosum

---
+++ twosum.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        storage = {}
+
+        for i in range(len(nums)):
+            if target - nums[i] in storage:
+                return [storage[target - nums[i]], i]
+                
+            else:
+                storage[nums[i]] = i
+
+            print(storage)
+
+            
+
+
+# nums = [2,7,11,15] 
+# target = 9
+
+nums = [3,2,4]
+target = 6
+
+# nums = [3,3]
+# target = 6
+
+sum = Solution()
+sum.twoSum(nums ,target)
+
+print(sum)
+
+        
